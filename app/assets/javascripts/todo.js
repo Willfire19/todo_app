c973a5c90e317488d46ca21aa65d407056a8123b
Willfire19 <willfire19@gmail.com>
2014-11-18 01:55:22 -0500

    Added material design to the todos

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -4,4 +4,8 @@ $(document).ready(function() {
 	$('.todo').has(".status:contains('In Progress')").addClass('status-InProgress');
 	$('.todo').has(".status:contains('To Do')").addClass('status-Todo');
 
+	$('.todo_material').has(".status_material:contains('Complete!')").addClass('status-MComplete');
+	$('.todo_material').has(".status_material:contains('In Progress')").addClass('status-MInProgress');
+	$('.todo_material').has(".status_material:contains('To Do')").addClass('status-MTodo');
+
 });
