76acb9755717876b84861c87bfab40362cf2ecc1
Willfire19 <willfire19@gmail.com>
2014-11-28 00:45:44 -0500

    Added sliding transitions for the todo sorting, and replaced the todo design on the home page to the one on the profile page

--- app/assets/javascripts/list_sort.js
+++ app/assets/javascripts/list_sort.js
@@ -3,13 +3,17 @@ $(document).ready(function() {
 	$('.list_sort').click(function() {
 		$('.list_sort').removeClass('active');
 		$(this).addClass('active');
-		$('.todo_material').fadeOut();
+		// $('.todo_material').fadeOut(200).delay(200);
+		// $('.todo_material').slideUp( "fast", "swing" ).delay(400);
+
 		var list_name = $(this).text();
 		if( list_name != "All" ){
-			$('.todo_material').has(".listName_material:contains('" + list_name + "')").fadeIn();
+			$('.todo_material').not(":has(.listName_material:contains('" + list_name + "'))").slideUp("fast", "swing");
+
+			$('.todo_material').has(".listName_material:contains('" + list_name + "')").slideDown("fast", "swing");
 		}
 		else{
-			$('.todo_material').fadeIn();
+			$('.todo_material').slideDown("fast", "swing");
 		}
 	})
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,7 @@ class HomeController < ApplicationController
 		if signed_in?
 			@user = current_user
     	@todos = @user.todos.paginate(page: params[:page])
+    	@lists = @user.lists
 			@todo = current_user.todos.build
 			@feed_items = current_user.feed.paginate(page: params[:page])
 		end
