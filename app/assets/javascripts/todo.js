30f2407df62b6f570d351f9e3449cb6c542fc77a
Joshua Angeley <willfire19@gmail.com>
2015-01-20 01:27:58 -0500

    Todos are now sorted into two groups: a group for complete todos and a group for todos to be completed

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,40 +1,32 @@
 jQuery.fn.todoStatus = function() {
-	// $('.todo_material').has(".status_material:contains('Complete!')").addClass('status-MComplete');
-	// $('.todo_material').has(".status_material:contains('In Progress')").addClass('status-MInProgress');
-	// $('.todo_material').has(".status_material:contains('To Do')").addClass('status-MTodo');
 
 	this.has(".status_material:contains('Complete!')").addClass('status-MComplete');
 	this.has(".status_material:contains('In Progress')").addClass('status-MInProgress');
 	this.has(".status_material:contains('To Do')").addClass('status-MTodo');
+
 }
 
 jQuery.fn.todoAdvanced = function() {
-	// $('.advanced').hide();
-
-	// $('.todo_material').click(function() {
-	// 	$(this).find('.advanced').slideToggle();
-	// });
 
 	this.find('.advanced').hide();
 
 	this.click(function() {
 		$(this).find('.advanced').slideToggle();
 	});
+
+}
+
+jQuery.fn.sortTodosByStatus = function() {
+	
+	this.has(".status_material:contains('Complete!')").insertAfter('#complete');
+	this.has(".status_material:contains('In Progress')").insertAfter('#todosToBeDone');
+	this.has(".status_material:contains('To Do')").insertAfter('#todosToBeDone');
 }
 
 $(document).ready(function() {
 
-	// $('.todo_material').has(".status_material:contains('Complete!')").addClass('status-MComplete');
-	// $('.todo_material').has(".status_material:contains('In Progress')").addClass('status-MInProgress');
-	// $('.todo_material').has(".status_material:contains('To Do')").addClass('status-MTodo');
-
-	// $('.advanced').hide();
-
-	// $('.todo_material').click(function() {
-	// 	$(this).find('.advanced').slideToggle();
-	// });
-
 	$('.todo_material').todoStatus();
+	$('.todo_card').sortTodosByStatus();
 	$('.todo_material').todoAdvanced();
 
 });
